dc1f4bfeb0cfc5eda3ba3ecccfed05fbb2785ad8
dharamaskoid <dhara.maskoid@gmail.com>
2026-05-08 16:34:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -157,7 +157,7 @@ const formatNumber = (num: number, suffix: string) => {
     <main className="bg-[#0B0F1A] text-white">
 
               {/* HERO SECTION */}
-              <section className="relative min-h-[800px] flex items-start md:items-center px-6 md:px-12 lg:px-24 pt-32 md:pt-0 pb-16">
+              <section className="relative min-h-[800px] flex items-start md:items-center px-6 md:px-12 lg:px-24 pt-32 md:pt-0">
 
                 <div className="container mx-auto grid md:grid-cols-2 gap-12 md:gap-16 items-center">
 
