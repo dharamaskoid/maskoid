6dd01cbd497694af179fa5c049b9a3eee8dab866
dharamaskoid <dhara.maskoid@gmail.com>
2026-05-08 15:26:46 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -119,7 +119,6 @@ export default function Header() {
       </div>
 
       {/* 🔥 ANIMATED MOBILE MENU */}
-      {/* 🔥 ANIMATED MOBILE MENU */}
       <AnimatePresence>
         {open && (
           <motion.div
