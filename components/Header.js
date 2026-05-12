0a035b179d769e883fbbbf956c603d5ac38a7bba
dharamaskoid <dhara.maskoid@gmail.com>
2026-05-12 11:20:39 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -190,7 +190,7 @@ export default function Header() {
                       <a
                         key={i}
                         href={service.link}
-                        className="block text-base text-[#2b4c9a] hover:text-white transition"
+                        className="block text-base text-[#6EA8FF] hover:text-white transition"
                       >
                         {service.name}
                       </a>
