c142aebddf1a2ddc77f5070f152bb1295b739977
dharamaskoid <dhara.maskoid@gmail.com>
2026-05-12 09:59:28 +0530

    Update TrustSection.jsx

--- components/TrustSection.jsx
+++ components/TrustSection.jsx
@@ -35,7 +35,7 @@ const logosRow2 = [
 
 const BrandMarquee = () => {
   return (
-    <section className="py-16 bg-[#928D87] overflow-hidden">
+    <section className="py-16 bg-[#EEF2FF] overflow-hidden">
 
         <div className="max-w-6xl mx-auto px-6 text-center mb-12">
 
