a543acc8dc469652122ea4087880a6e890de57b4
dharamaskoid <dhara.maskoid@gmail.com>
2026-05-13 10:21:14 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -878,17 +878,7 @@ const formatNumber = (num: number, suffix: string) => {
                 className="relative py-10 bg-[#EEF4FF] overflow-hidden z-20 border-y border-[#dbe7ff]"
               >
 
-              {/* 🌌 MOVING BACKGROUND ELEMENTS */} 
-              <div className="absolute inset-0 overflow-hidden pointer-events-none z-0"> 
-                
-                {/* BIG BLUE DRIFT */} 
-                <div className="absolute w-[500px] h-[500px] bg-blue-500/10 rounded-full blur-3xl animate-drift1"></div> 
-                
- 
-                
-                {/* DOT GRID MOTION */} 
-                <div className="absolute inset-0 opacity-20 bg-[radial-gradient(circle,#1a2e5e_1px,transparent_1px)] [background-size:40px_40px] animate-gridMove"></div>
-              </div>
+
 
                 <div className="max-w-7xl mx-auto px-6 relative z-10">
 
