a7b664df09afe0a3d0e45ceeada45eb3fb45e5bc
dharamaskoid <dhara.maskoid@gmail.com>
2026-05-16 15:34:34 +0530

    Update page.tsx

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -332,16 +332,16 @@ export default function ContactPage() {
 
         <div className="container mx-auto overflow-hidden rounded-2xl border border-[#dbe7ff] shadow-[0_20px_60px_rgba(43,76,154,0.08)]">
 
-            <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d117421.67139215356!2d72.45714186361182!3d23.020497753422073!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e84f66b0d1b93%3A0x3c0d3bdbfbb27417!2sAhmedabad%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1710000000000!5m2!1sen!2sin"
-            width="100%"
-            height="450"
-            style={{ border: 0 }}
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-            className="w-full"
-            ></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6267.920822954264!2d73.17486261049172!3d22.307488551278368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395fc890c85ccb85%3A0x79f808e425688eb9!2sMaskoid%20Technologies!5e0!3m2!1sen!2sin!4v1778925243270!5m2!1sen!2sin"
+                    width="100%" 
+                    height="450" 
+                    style={{border:0}} 
+                    allowFullScreen 
+                    loading="lazy" 
+                    referrerPolicy="no-referrer-when-downgrade"
+                    className="w-full"
+                    >
+                    </iframe>
 
         </div>
 
