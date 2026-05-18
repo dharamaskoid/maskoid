7f8a298fd73dd20edd12f1ff3326a4780dd8b150
dharamaskoid <dhara.maskoid@gmail.com>
2026-05-18 09:51:43 +0530

    Update page.tsx

--- app/services/[slug]/page.tsx
+++ app/services/[slug]/page.tsx
@@ -13,6 +13,9 @@ export default function ServicePage({ params }: Props) {
 
   switch (params.slug) {
 
+    case "web-development":
+      return <WebDevelopmentPage />;
+
     case "web-design":
       return <WebDesignPage />;
 
@@ -21,9 +24,6 @@ export default function ServicePage({ params }: Props) {
 
     case "performance-marketing":
       return <MarketingPage />;
-
-    case "web-development":
-      return <WebDevelopmentPage />;
 
     default:
       notFound();
