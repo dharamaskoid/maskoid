2e72115f1471cee17a6f0b00c40c792b2d7ecf40
dharamaskoid <dhara.maskoid@gmail.com>
2026-05-18 10:13:11 +0530

    Create page.tsx

---
+++ app/services/services/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import { notFound } from "next/navigation";
+
+import WebDevelopmentPage from "@/components/services/WebDevelopmentPage";
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default function ServicePage({ params }: Props) {
+
+  switch (params.slug) {
+
+    case "web-development":
+      return <WebDevelopmentPage />;
+
+    default:
+      notFound();
+  }
+}
