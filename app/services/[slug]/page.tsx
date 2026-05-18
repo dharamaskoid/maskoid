b7156be938ffe4541e804a4214110df218eef175
dharamaskoid <dhara.maskoid@gmail.com>
2026-05-18 10:18:15 +0530

    Update page.tsx

--- app/services/[slug]/page.tsx
+++ app/services/[slug]/page.tsx
@@ -1,21 +1,15 @@
 import { notFound } from "next/navigation";
-
 import WebDevelopmentPage from "@/components/services/WebDevelopmentPage";
 
-type Props = {
-  params: {
-    slug: string;
-  };
-};
+export default function ServicePage({
+  params,
+}: {
+  params: { slug: string };
+}) {
 
-export default function ServicePage({ params }: Props) {
-
-  switch (params.slug) {
-
-    case "web-development":
-      return <WebDevelopmentPage />;
-
-    default:
-      notFound();
+  if (params.slug === "web-development") {
+    return <WebDevelopmentPage />;
   }
+
+  return notFound();
 }
