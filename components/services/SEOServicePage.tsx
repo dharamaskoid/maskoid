86cb6cd86e2059edb98493f2e19c9c4822c53c08
dharamaskoid <dhara.maskoid@gmail.com>
2026-05-22 11:41:05 +0530

    Update SEOServicePage.tsx

--- components/services/SEOServicePage.tsx
+++ components/services/SEOServicePage.tsx
@@ -1045,7 +1045,7 @@ export default function SEOServicePage() {
       </section>
 
       {/* CTA */}
-      <section className="pb-24 px-6 md:px-10 lg:px-16">
+      <section className="py-16 px-6 lg:px-16">
 
         <div className="container mx-auto bg-[#1a2e5e] rounded-xl p-10 md:p-16 relative overflow-hidden">
 
